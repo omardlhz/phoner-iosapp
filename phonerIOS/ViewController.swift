8d6975d59adfc84f26d3334cc7b90fb1b00aedc6
omardlhz <omar.dlhz@hotmail.com>
2016-01-11 16:59:36 -0500

    Client app connects successfully to server app.

--- phonerIOS/ViewController.swift
+++ phonerIOS/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCNearbyServiceAdvertiserDelegate, MCSessionDelegate{
+    
+    var peerID: MCPeerID!
+    var serviceAdvertiser: MCNearbyServiceAdvertiser!
+    var mcSession: MCSession!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        
+        self.serviceAdvertiser = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: "phoner")
+        self.serviceAdvertiser.delegate = self
+        self.serviceAdvertiser.startAdvertisingPeer()
+        
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .Required)
+        mcSession.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +34,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+        return
+    }
+    
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+        
+        return
+    }
+    
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+        
+        return
+    }
+    
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        
+        print("\(peerID.displayName) changed state: \(state.toString())")
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        
+        return
+    }
+    
+    
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: NSError) {
+        
+        NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
+    }
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: NSData?, invitationHandler: (Bool, MCSession) -> Void) {
+        
+        NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
+        invitationHandler(true, self.mcSession)
+    }
 
 }
 
+extension MCSessionState {
+    func toString() -> String {
+        switch self {
+        case .Connected:    return "Connected"
+        case .Connecting:   return "Connecting"
+        case .NotConnected: return "Not Connected"
+        }
+    }
+}
